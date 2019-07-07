713d9bb7fc541b2e06df5a8d8400d341b1deefe9
Konstantin Savchenko <killerkennyx41@gmail.com>
2019-07-07 17:55:06 +0300

    Updates

---
+++ precache-manifest.859e3162a489c4aadc2dc441f47a9324.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "289cdcec24d5b4257df8",
+    "url": "/react-pdf-reader/static/css/main.e2ba8f57.chunk.css"
+  },
+  {
+    "revision": "289cdcec24d5b4257df8",
+    "url": "/react-pdf-reader/static/js/main.289cdcec.chunk.js"
+  },
+  {
+    "revision": "e99982403b77278461a6",
+    "url": "/react-pdf-reader/static/js/1.e9998240.chunk.js"
+  },
+  {
+    "revision": "1a0cd85ccc7f7113492b",
+    "url": "/react-pdf-reader/static/js/runtime~main.1a0cd85c.js"
+  },
+  {
+    "revision": "f81b0e5beeabfa19b9f0d1be8ac3fa4d",
+    "url": "/react-pdf-reader/index.html"
+  }
+];
